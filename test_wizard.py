f1c60a2d52541eb4840c397a728910d654b6e448
bryanjangeesingh <bryanjangeesingh@gmail.com>
2024-11-23 02:20:29 -0500

    wip

---
+++ test_wizard.py
@@ -0,0 +1,12 @@
+# load wizardcoder model
+from transformers import AutoModelForCausalLM, AutoTokenizer
+model = AutoModelForCausalLM.from_pretrained("WizardLMTeam/WizardCoder-Python-13B-V1.0")
+tokenizer = AutoTokenizer.from_pretrained("WizardLMTeam/WizardCoder-Python-13B-V1.0")
+
+# pass in prompt
+prompt = "def hello_world():"
+input_ids = tokenizer(prompt, return_tensors="pt").input_ids
+outputs = model.generate(input_ids, max_length=100, num_return_sequences=1, early_stopping=True)    
+generated_text = tokenizer.decode(outputs[0], skip_special_tokens=True)
+import pdb; pdb.set_trace()
+print(generated_text)
