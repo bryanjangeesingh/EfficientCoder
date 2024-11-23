e4dcf09d2f979fbb4eaed2383cb1e0e6747e61b6
bryanjangeesingh <bryanjangeesingh@gmail.com>
2024-11-23 02:34:47 -0500

    wip

--- test_wizard.py
+++ test_wizard.py
@@ -79,7 +79,7 @@ problem = {"task_id": "test/0", "prompt": "def return1():\n", "canonical_solutio
 def main():
     # Test prompt
     test_prompt = (
-        "Below is the start of a python program. Complete it.\n\n"
+        "Below is the start of a python program. Continue it.\n\n"
         "### Instruction:\n"
         f"{problem['prompt']}\n\n"
         "### Response:\n"
